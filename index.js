ac7e699d79bbb48d6ab8a1c106016695cabf8e2d
AguenonN <junnoudogbessi@gmail.com>
2026-02-09 15:22:44 +0100

    printing pokemons

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import pokemon from './schema/pokemon.js';
 
 import './connect.js'
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
